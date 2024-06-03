c8555ea0a53b1b78a3fcedd454f1a940e964c565
HE-22 <134304564+HE-22@users.noreply.github.com>
2024-06-03 23:49:16 +0300

    update port value in docker

--- server.py
+++ server.py
@@ -45,5 +45,6 @@ def detect_languages_api():
     return jsonify({"error": "Unexpected error"}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5003))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port, debug=True)
+
